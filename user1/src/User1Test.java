15adf418d6b04b0cf0379e72d8d7cd8238474181
sparkChan <sparkchan@gmail.com>
2020-02-27 10:53:45 +0800

    remove all

--- user1/src/User1Test.java
+++ user1/src/User1Test.java
@@ -4,13 +4,5 @@
  */
 public class User1Test {
     public static void main(String[] args) {
-        System.out.println("User 1");
-        System.out.println("Repair bug 101");
-        System.out.println("Repair bug 102");
-        System.out.println("User 2");
-        System.out.println("Merge test");
-        System.out.println("Merge test2");
-        System.out.println("Merge test3");
-        System.out.println("Fix bug 103");
     }
 }
