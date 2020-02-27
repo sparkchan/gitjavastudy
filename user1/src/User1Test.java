7c996ebaa12d81bdf27fb10b8807b15a62497f31
sparkChan <sparkchan@gmail.com>
2020-02-27 09:43:49 +0800

    merge test2

--- user1/src/User1Test.java
+++ user1/src/User1Test.java
@@ -8,5 +8,6 @@ public class User1Test {
         System.out.println("Repair bug 101");
         System.out.println("Repair bug 102");
         System.out.println("User 2");
+        System.out.println("Merge test2");
     }
 }
